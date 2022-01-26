2ccda968950762bdc946d450edc4672b9b3c072a
Fraser Irvine <1801054@rgu.ac.uk>
2022-01-26 14:45:11 +0000

    converted project to work on ejs

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
+//new code using express
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+var router = express.Router();
+
+app.set('view engine', 'ejs');
+
 const PORT = process.env.PORT || 8080;
 
 var port = PORT;
 
+//render the page in ejs
+app.get('/', function(req,res){
+    res.render('index.ejs');
+});
+
+
+
+/**
+//old code for rendering page in .html
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
+/*
+
+//getting information from the search form
+router.post('/search', function(req,res,next) {
+    console.log(req.body);
+});
+
+//getting information from search form (old code)
+/**
+app.post('/search', function(req,res){
+    console.log("search has been performed");
+});
+*/
 
 app.listen(PORT);
 console.log('Express server running at http://127.0.0.1:'.PORT);
 
 
+
+
+
+//old code being commented out
 /*const http = require('http');
 const fs = require('fs');
 const express = require('express');
